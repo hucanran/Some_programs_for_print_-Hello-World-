035e9769725228d5c4d542095660c6c29d92aec6
胡粲冉 <83101852+hucanran@users.noreply.github.com>
2022-04-13 17:43:05 +0800

    Update Hello World.cpp

--- Hello World.cpp
+++ Hello World.cpp
@@ -1,7 +1,7 @@
-#include<iostream>
-using namespace std;
+#include<iostream> //导入标准输入输出（控制台）库
+using namespace std; // 使用命名空间std内的内容
 
-int main(){
-  cout<<"Hello World!";
-  return 0;
+int main(){ // 主函数
+  cout<<"Hello World!";// 打印
+  return 0;// 返回
 }
